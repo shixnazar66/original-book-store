b4d9285f883c603240dc98c2a666dfdd6d57a506
shixnazar66 <vaisovshixnazar66@gmail.com>
2023-11-28 17:53:50 +0500

    new

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './—no-spec/module/auth/auth.module';
 import { UserModule } from './module/user/user.module';
 import { AuthModule } from './module/auth/auth.module';
-import { AuthModule } from './—no-spec/module/auth/auth.module';
+
 
 @Module({
   imports: [AuthModule, UserModule],
--- src/module/user/entities/user.entity.ts
+++ src/module/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Rootentity } from 'src/common/entity/root.entity';
+import { UserRole } from 'src/common/enum/user-role.enum';
+import { Entity, Column, ManyToMany, JoinTable } from 'typeorm';
+
+@Entity()
+export class User extends Rootentity {
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ enum: UserRole, type: 'enum', default: UserRole.user })
+  role: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  refresh_token: string;
+}
